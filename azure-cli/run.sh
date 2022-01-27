66737ad352cbb187ca49be2c2388331c880d7d7b
Jamison Kissh <jak119@gmail.com>
2022-01-27 09:31:35 -0600

    Add timeout_value

--- azure-cli/run.sh
+++ azure-cli/run.sh
@@ -6,9 +6,16 @@ CONTAINER="$(bashio::config 'containername')"
 bashio::log "Found these backups:"
 ls /backup/
 
+params=()
+if bashio::config.has_value "timeout_value"
+then
+    bashio::log "Timeout is specified"
+    params+=(--timeout $(bashio::config 'timeout_value'))
+fi
+
 bashio::log "=== Starting copy ==="
 bashio::color.blue
-az storage blob upload-batch --connection-string "$(bashio::config 'connectionstring')" -d $CONTAINER -s /backup/ --no-progress
+az storage blob upload-batch --connection-string "$(bashio::config 'connectionstring')" -d $CONTAINER -s /backup/ --no-progress $params
 
 bashio::color.black
 bashio::log "=== Finished copy ==="
