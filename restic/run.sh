f1ac6b521ca7f6c0e93623204204b63e60573946
Jamison Kissh <jak119@gmail.com>
2022-02-17 18:19:28 +0000

    Tweak script

--- restic/run.sh
+++ restic/run.sh
@@ -5,7 +5,7 @@ export AZURE_ACCOUNT_NAME="$(bashio::config 'storageaccount')"
 export AZURE_ACCOUNT_KEY="$(bashio::config 'key')"
 CONTAINER="$(bashio::config 'containername')"
 FOLDER="$(bashio::config 'folder')"
-RESTIC_REPOSITORY="azure:"${CONTAINER}":/"${FOLDER}
+RESTIC_REPOSITORY="azure:${CONTAINER}:/${FOLDER}"
 
 # Function Definitions
 
@@ -17,7 +17,7 @@ function initializeRepository() {
     fi
 
     bashio::log "Checking repo password"
-    bashio::pwned.is_safe_password($(bashio::config 'repopassword'))
+    bashio::pwned.is_safe_password "$(bashio::config 'repopassword')"
 
     bashio::log "Initializaing repository at ${RESTIC_REPOSITORY} ... "
     restic -p ./password.txt init --repo ${RESTIC_REPOSITORY} > /dev/null
@@ -32,7 +32,7 @@ function runBackup() {
 
 function pruneOldBackups() {
     bashio::log "Pruning old backups"
-    restic -p ./password.txt --repo ${RESTIC_REPOSITORY} --keep-daily "$(bashio::config 'days_to_keep')"
+    restic -p ./password.txt --repo ${RESTIC_REPOSITORY} forget --keep-daily "$(bashio::config 'days_to_keep')"
     bashio::log.green "Pruning completed"
 }
 
