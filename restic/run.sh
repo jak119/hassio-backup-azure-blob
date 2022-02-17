dbcc81aa1ae87b8ef4d05e2b6fb0e9fe3161af81
Jamison Kissh <jak119@gmail.com>
2022-02-16 22:51:39 -0600

    Write initial script

--- restic/run.sh
+++ restic/run.sh
@@ -5,9 +5,38 @@ export AZURE_ACCOUNT_NAME="$(bashio::config 'storageaccount')"
 export AZURE_ACCOUNT_KEY="$(bashio::config 'key')"
 CONTAINER="$(bashio::config 'containername')"
 FOLDER="$(bashio::config 'folder')"
+RESTIC_REPOSITORY="azure:"${CONTAINER}":/"${FOLDER}
+
+# Function Definitions
+
+function initializeRepository() {
+    # Inspired by https://github.com/PHLAK/restic-init/
+    if restic -p ./password.txt --no-cache --repo ${RESTIC_REPOSITORY} snapshots &> /dev/null; then
+        echo "Respository already intialized at ${RESTIC_REPOSITORY}"
+        return 0
+    fi
+
+    echo -n "Initializaing repository at ${RESTIC_REPOSITORY} ... "
+    restic -p ./password.txt init --repo ${RESTIC_REPOSITORY} > /dev/null
+    echo "DONE"
+}
+
+function runBackup() {
+    restic -p ./password.txt --repo ${RESTIC_REPOSITORY} backup /backup/
+}
+
+function pruneOldBackups() {
+    restic -p ./password.txt --repo ${RESTIC_REPOSITORY} --keep-daily "$(bashio::config 'days_to_keep')"
+}
+
+# Create a password file
+echo "$(bashio::config 'repopassword')" >> ./password.txt
 
 # Check for a Restic repo, initialize if it doesn't exist
+initializeRepository
 
 # Then backup
+runBackup
 
 # Cleanup old snapshots
+pruneOldBackups
