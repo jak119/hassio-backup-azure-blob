12021ff72dce96d1d89d837cee5ad0bec1b4f166
Jamison Kissh <jak119@gmail.com>
2022-09-24 17:53:43 +0000

    Change shell and use new bashio features

--- azure-cli/run.sh
+++ azure-cli/run.sh
@@ -1,23 +1,20 @@
-#!/usr/bin/env bashio
+#!/command/with-contenv bashio
 
-CONFIG_PATH=/data/options.json
 CONTAINER="$(bashio::config 'containername')"
 
-bashio::log "Found these backups:"
-ls /backup/
+bashio::log.info "Found these backups:"
+bashio::log.info "$(ls /backup/)"
 
 params=()
 if bashio::config.has_value "timeout_value"
 then
-    bashio::log "Timeout value of $(bashio::config 'timeout_value') is specified"
+    bashio::log.info "Timeout value of $(bashio::config 'timeout_value') is specified"
     params+=(--timeout "$(bashio::config 'timeout_value')")
 fi
 
-bashio::log "=== Starting copy ==="
-bashio::color.blue
+bashio::log.info "=== Starting copy ==="
 # az storage blob upload-batch --connection-string "$(bashio::config 'connectionstring')" -d $CONTAINER -s /backup/ --no-progress $params
 az storage blob upload-batch --connection-string "$(bashio::config 'connectionstring')" -d $CONTAINER -s /backup/ --no-progress
 
-bashio::color.black
-bashio::log "=== Finished copy ==="
+bashio::log.info "=== Finished copy ==="
 bashio::exit.ok
