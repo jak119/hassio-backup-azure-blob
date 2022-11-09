000a484395dca558178fc8f08fa56ccd4d344204
Jamison Kissh <jak119@gmail.com>
2022-11-08 21:48:51 -0500

    Add Python files

--- azure-cli-dev/run.sh
+++ azure-cli-dev/run.sh
@@ -1,20 +1,13 @@
 #!/command/with-contenv bashio
 
 CONTAINER="$(bashio::config 'containername')"
+CONNECTION_STRING="$(bashio::config 'connectionstring')"
 
 bashio::log.info "Found these backups:"
 bashio::log.info "$(ls /backup/)"
 
-params=()
-if bashio::config.has_value "timeout_value"
-then
-    bashio::log.info "Timeout value of $(bashio::config 'timeout_value') is specified"
-    params+=(--timeout "$(bashio::config 'timeout_value')")
-fi
+bashio::log.info "Starting Python script"
 
-bashio::log.info "=== Starting copy ==="
-# az storage blob upload-batch --connection-string "$(bashio::config 'connectionstring')" -d $CONTAINER -s /backup/ --no-progress $params
-az storage blob upload-batch --connection-string "$(bashio::config 'connectionstring')" -d $CONTAINER -s /backup/ --no-progress
+python backup.py
 
-bashio::log.info "=== Finished copy ==="
 bashio::exit.ok
