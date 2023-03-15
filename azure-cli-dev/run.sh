5abd434417d107d4fba16edac5733fcbd607ed5d
Jamison Kissh <jak119@gmail.com>
2023-03-15 00:04:23 -0400

    Refresh from current version

--- azure-cli-dev/run.sh
+++ azure-cli-dev/run.sh
@@ -1,13 +1,20 @@
 #!/command/with-contenv bashio
 
 CONTAINER="$(bashio::config 'containername')"
-CONNECTION_STRING="$(bashio::config 'connectionstring')"
 
 bashio::log.info "Found these backups:"
 bashio::log.info "$(ls /backup/)"
 
-bashio::log.info "Starting Python script"
+params=()
+if bashio::config.has_value "timeout_value"
+then
+    bashio::log.info "Timeout value of $(bashio::config 'timeout_value') is specified"
+    params+=(--timeout "$(bashio::config 'timeout_value')")
+fi
 
-python backup.py
+bashio::log.info "=== Starting copy ==="
+# az storage blob upload-batch --connection-string "$(bashio::config 'connectionstring')" -d $CONTAINER -s /backup/ --no-progress $params
+az storage blob upload-batch --connection-string "$(bashio::config 'connectionstring')" -d $CONTAINER -s /backup/ --no-progress
 
+bashio::log.info "=== Finished copy ==="
 bashio::exit.ok
