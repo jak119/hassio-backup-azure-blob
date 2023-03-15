8d912e49b12963abe3fa0f0feb91f6793e2922c8
Jamison Kissh <jak119@gmail.com>
2023-03-14 23:54:31 -0400

    Remove []

--- azcopy/run.sh
+++ azcopy/run.sh
@@ -3,7 +3,7 @@
 SAS="$(bashio::config 'sas_token')"
 
 params=()
-if [[ $(bashio::config.true 'mirror') ]]; then
+if $(bashio::config.true 'mirror'); then
     bashio::config.suggest.false 'mirror' 'This will cause backups to be deleted from Azure'
 
     params+=(--mirror-mode=true)
