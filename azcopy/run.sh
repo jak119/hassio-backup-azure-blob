977a31f9982dc054f1bdae2eecfb312b7c1f2b9d
Jamison Kissh <jak119@gmail.com>
2023-03-21 22:18:34 -0400

    Add date in ISO8601 format

--- azcopy/run.sh
+++ azcopy/run.sh
@@ -16,11 +16,11 @@ params+=(--put-md5)
 
 bashio::log.info "$(azcopy --version)"
 
-bashio::log.info "Starting Azcopy"
+bashio::log.info "Starting Azcopy at $(date +"%Y-%m-%dT%H:%M:%S%z")"
 azcopy sync "/backup" "$SAS" "${params[@]}"
 
 if [ $? -eq 0 ]; then
-    bashio::log.info "Azcopy successfully completed"
+    bashio::log.info "Azcopy successfully completed at $(date +"%Y-%m-%dT%H:%M:%S%z")"
     bashio::exit.ok
 else
     bashio::exit.nok "There was an error with Azcopy"
